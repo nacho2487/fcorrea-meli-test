0d955cf90b94009bb11def28cac43bcffa267cb2
Franco Correa <franco@francocorrea.com>
2018-11-27 21:42:47 -0300

    Base pages

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+import Head from "../components/head";
+import { Navigation } from "../components/ui";
+
+const Home = () => (
+  <div>
+    <Head title="Home" />
+    <Navigation />
+  </div>
+);
+
+export default Home;
